86f72d0ebd848839d1e035af280a9592e2b18208
mayoha <mayoha@163.com>
2025-08-06 02:17:42 +0800

    完善目录插件

--- popup.js
+++ popup.js
@@ -1,16 +1,26 @@
+import { addToc } from './toc/toc.js';
+let isAddStyle = false;
 // 生成目录
 document.getElementById("toggleButton").addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: addToc,
+  });
   // 注入CSS文件
-  await chrome.scripting.insertCSS({
-    target: { tabId: tab.id },
-    files: ["toc/toc.css"] // 相对于manifest.json的路径
-  });
-  // 在当前标签页执行脚本
-  await chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['toc/toc.js']
-  });
+  if (!isAddStyle) {
+    chrome.scripting.insertCSS({
+      target: { tabId: tab.id },
+      files: ["toc/toc.css"]
+    });
+    isAddStyle = true;
+  } else {
+    chrome.scripting.removeCSS({
+      target: { tabId: tab.id },
+      files: ["toc/toc.css"]
+    });
+    isAddStyle = false;
+  }
 });
 
 // 签章
