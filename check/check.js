15ce8dfb0ff016a84efc08ac6e773ef9a8cb6d86
v_mayonghua <v_mayonghua@baidu.com>
2025-08-07 10:21:16 +0800

    完成签章功能

--- check/check.js
+++ check/check.js
@@ -1,1 +1,14 @@
-alert('签章');
+const checkboxs = document.querySelectorAll('input[type=checkbox]')
+
+for(let node of checkboxs) {
+    node.click();
+}
+
+const radio = document.querySelector('input[type=radio]')
+radio.click()
+
+const submitBtn = document.querySelector('.cxd-Button')
+submitBtn.scrollIntoView({
+    behavior: 'smooth'
+});
+submitBtn.click();
